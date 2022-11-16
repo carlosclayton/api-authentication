236fb682a014ed163534c618f166aff5b55b4edc
Carlos Clayton <carlos.clayton@gmail.com>
2022-11-16 17:14:32 -0300

    feat: middleware is authentication

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,8 +2,10 @@ import {Router} from "express";
 
 const userRouter = Router();
 import {UsersController} from "../controllers/UsersController";
+import {IsAuthentication} from "../middlewares/isAuthentication";
 const userController = new UsersController();
 
+userRouter.use(IsAuthentication)
 userRouter.post("/", userController.create);
 userRouter.get("/", userController.all);
 userRouter.get("/:id", userController.find);
