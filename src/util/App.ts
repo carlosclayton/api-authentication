225c41e94a858a9b70f900c7db5028232fb2f705
Carlos Clayton <carlos.clayton@gmail.com>
2022-12-07 10:37:55 -0300

    feat: sentry.io integration

--- src/util/App.ts
+++ src/util/App.ts
@@ -7,14 +7,37 @@ import swaggerFile from "../swagger.json"
 import {NextFunction, Request, request, Response, response} from "express";
 import upload from "./Upload";
 import raterLimiter from "../middlewares/raterLimiter";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 
 const app = express();
 app.use(raterLimiter);
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+
 app.use(express.json())
 app.use(routes)
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 
-
+app.use(Sentry.Handlers.errorHandler());
 app.use((
     err: Error, request: Request, response: Response, next: NextFunction
 ) => {
